bc2ec12d4c666776da2fcd04aad77d8fdf3e63e8
16602724475 <1714797713@qq.com>
2022-09-20 07:49:55 +0800

    init

---
+++ vue-practise/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const randomNumber = ref((Math.random() * 100).toFixed(2))
+
+onMounted(()=>{
+  const Timer =  setInterval(()=>{
+    console.log(123);
+    randomNumber.value = (Math.random() * 100).toFixed(2)
+  },2000)
+})
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+
+  <div class="random">
+    <h1>{{ randomNumber }}%</h1>
+  </div>
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
